cd72f2589f29f9fc825ae210e4529b0db1d4dfd1
adbiriukov <git config --global user.email you@example.com>
2021-01-07 21:05:25 +0500

    add tests 22, 23

--- test_22_how_to_read_dataFrom_msExcel_using_OpenPyXL.py
+++ test_22_how_to_read_dataFrom_msExcel_using_OpenPyXL.py
@@ -1,6 +1,6 @@
 import openpyxl
 
-path = "C:/Users/User/Downloads/111.xlsx"
+path = "./selenium_logs/xlsx_files/111.xlsx"
 
 workbook = openpyxl.load_workbook(path)
 sheet = workbook.active  # extract 1 sheet if you have multiple sheets, you must use "get_sheet_by_name(sheet1)"
@@ -8,8 +8,9 @@ sheet = workbook.active  # extract 1 sheet if you have multiple sheets, you must
 # how many rows and columns we have. To get number of
 rows = sheet.max_row
 columns = sheet.max_column
-# print(rows) # 3
-# print(columns) # 4
+print("Number of rows == " + str(rows))
+print("Number of columns == " + str(columns))
+print("--------")
 
 for r in range(1, rows+1):
     for c in range(1, columns+1):
