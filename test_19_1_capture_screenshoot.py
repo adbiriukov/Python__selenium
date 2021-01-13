42970c1f267219396059060b1ee9cec7689ae346
adbiriukov <git config --global user.email you@example.com>
2021-01-14 00:37:37 +0500

    add some tests

--- test_19_1_capture_screenshoot.py
+++ test_19_1_capture_screenshoot.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path="C:/Users/User/PycharmProjects/last_try/pytest_start_2/chromedriver.exe")
+driver = webdriver.Chrome(executable_path="./chromedriver.exe")
 driver.get("https://www.google.com/")
 driver.maximize_window()
 # Method 1
-# driver.save_screenshot("C:/Users/User/Downloads/homepage.png")
+# driver.save_screenshot("./selenium_logs/screenshots/homepage.png")
 
 # Method 2
-driver.get_screenshot_as_file("C:/Users/User/Downloads/homepage2.png")
+driver.get_screenshot_as_file("./selenium_logs/screenshots/homepage2.png")
 
 # To take full page screenshot
 # JavaScript executor with starting node of DOM and the height [Note: Make sure the browser in headless mode ]
