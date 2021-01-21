ef31b2c7afc747b79bd7df9afddd0b1b0dba6237
adbiriukov <git config --global user.email you@example.com>
2021-01-21 22:49:21 +0500

    -

--- 1_Selenium/test_11_conditional_commands.py
+++ 1_Selenium/test_11_conditional_commands.py
@@ -1,20 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-import time
-
-driver = webdriver.Chrome(executable_path="../chromedriver.exe")
-driver.get("https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407")
-driver.maximize_window()
-
-time.sleep(3)
-action = ActionChains(driver)
-cbox = driver.find_element_by_id('RESULT_CheckBox-8_0')
-print(cbox.is_selected())
-action.click(cbox).perform()
-
-print(cbox.is_enabled())     #
-print(cbox.is_selected())    # all return true/false based on element status
-print(cbox.is_displayed())   #
-
-
-driver.close()
+# from selenium import webdriver
+# from selenium.webdriver import ActionChains
+# import time
+#
+# driver = webdriver.Chrome(executable_path="../chromedriver.exe")
+# driver.get("https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407")
+# driver.maximize_window()
+#
+# time.sleep(3)
+# action = ActionChains(driver)
+# cbox = driver.find_element_by_id('RESULT_CheckBox-8_0')
+# print(cbox.is_selected())
+# action.click(cbox).perform()
+#
+# print(cbox.is_enabled())     #
+# print(cbox.is_selected())    # all return true/false based on element status
+# print(cbox.is_displayed())   #
+#
+#
+# driver.close()
