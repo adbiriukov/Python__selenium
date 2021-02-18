dd35bc3e2ff67fcf5888cb56c45f7dc6c0796175
adbiriukov <git config --global user.email you@example.com>
2021-02-18 16:13:55 +0500

    add practice 3

---
+++ 7_practice/3_practice/2.py
@@ -0,0 +1,27 @@
+import unittest
+from selenium import webdriver
+from amazon_web_elements import WebElements
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+driver = webdriver.Chrome(executable_path="../../chromedriver.exe")
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get("https://www.amazon.com/")
+
+
+def test_css_for_home_page(self):
+    self.assertTrue("http://g-ec2.images-amazon.com/images/G/01/social/api-share/amazon_logo_500500._V323939215_.png" in
+                    self.driver.find_element_by_css_selector("div")
+                    .value_of_css_property("background-image"))
+
+
+
+
+
+
+
+
+
+driver.close()
+
