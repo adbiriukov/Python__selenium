66ff187a377c66993617c91d8b738ce0beeb83a7
adbiriukov <git config --global user.email you@example.com>
2021-03-01 13:40:16 +0500

    add selenium_basic_commands.pu and added info to handle browser windows

--- 1_Selenium/test_7_how_to_handle_browser_windows.py
+++ 1_Selenium/test_7_how_to_handle_browser_windows.py
@@ -17,3 +17,26 @@ for handle in handles:
 
 
 driver.quit()  # Quit will close all windows (close will close only 1 window)
+
+
+# ## Multiple tabs/windows ##
+# #
+# # first window and tab are same, second every widow has a handle that can be used
+# # to switch to that window, we can perform actions only in current window
+# driver.get('http://demo.automationtesting.in/Windows.html')
+# # show the handle of our current focused window
+# print('current window handle', driver.current_window_handle)
+# time.sleep(3)
+#
+# # show the handles of all windows in our browser, firstly let's open a new window
+# driver.find_element(By.XPATH, '//*[@id="Tabbed"]/a/button').click()
+# print('all handles', driver.window_handles)
+#
+# # so once we obtained the window handle, we can switch to it and can fetch like a new page or perform other action
+# print("current window title:", driver.title)
+#
+# sec_window_handle = driver.window_handles[1]
+# driver.switch_to.window(sec_window_handle)
+# print("switched to second window:", driver.title)
+# # closes only focued window
+# driver.close()
