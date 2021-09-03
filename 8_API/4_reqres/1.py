b0ee891b87165e1deb7339d8e0ca7aaa12f875db
adbiriukov <adbiriukov@gmail.com>
2021-09-03 21:51:09 +0500

    Added more reqres tests

--- 8_API/4_reqres/1.py
+++ 8_API/4_reqres/1.py
@@ -1,29 +1,29 @@
 import pytest
 
 
-# @pytest.mark.parametrize('api_request', [['api/users?page=2', '201'], ['api/users/2', '201'],
-#                                          ['api/users/23', '404'], ['api/unknown', '200'],
-#                                          ['https://reqres.in/api/unknown/2', '201'], ['api/unknown/23', '404']])
-# def test_api_summation(api_client_get_status, api_request):
-#     response = api_client_get_status
-#     if response is True:
-#         assert True
+@pytest.mark.parametrize('api_request', [['api/users?page=2', '201'], ['api/users/2', '201'],
+                                         ['api/users/23', '404'], ['api/unknown', '200'],
+                                         ['https://reqres.in/api/unknown/2', '201'], ['api/unknown/23', '404']])
+def test_api_summation(api_client_get_status, api_request):
+    response = api_client_get_status
+    if response is True:
+        assert True
 
 
-# @pytest.mark.parametrize('api_request', [['api/users?page=2', "data", '0', 'id', '7'],
-#                                          ['api/users?page=2', "data", '0', 'email', 'michael.lawson@reqres.in'],
-#                                          ['api/users?page=2', "data", '0', 'first_name', 'Michael'],
-#                                          ['api/users?page=2', "data", '0', 'last_name', 'Lawson'],
-#                                          ['api/users?page=2', "data", '0', 'avatar', 'https://reqres.in/img/faces/7-image.jpg'],
-#                                          ['api/users?page=2', "data", '1', 'id', '8']
-#                                          ])
-# def test_api_summation(api_client_get_value, api_request):
-#     api_response = api_client_get_value
-#     try:
-#         int(api_request[4])
-#         assert api_response[api_request[1]][int(api_request[2])][api_request[3]] == int(api_request[4])
-#     except ValueError:
-#         assert api_response[api_request[1]][int(api_request[2])][api_request[3]] == api_request[4]
+@pytest.mark.parametrize('api_request', [['api/users?page=2', "data", '0', 'id', '7'],
+                                         ['api/users?page=2', "data", '0', 'email', 'michael.lawson@reqres.in'],
+                                         ['api/users?page=2', "data", '0', 'first_name', 'Michael'],
+                                         ['api/users?page=2', "data", '0', 'last_name', 'Lawson'],
+                                         ['api/users?page=2', "data", '0', 'avatar', 'https://reqres.in/img/faces/7-image.jpg'],
+                                         ['api/users?page=2', "data", '1', 'id', '8']
+                                         ])
+def test_api_summation(api_client_get_value, api_request):
+    api_response = api_client_get_value
+    try:
+        int(api_request[4])
+        assert api_response[api_request[1]][int(api_request[2])][api_request[3]] == int(api_request[4])
+    except ValueError:
+        assert api_response[api_request[1]][int(api_request[2])][api_request[3]] == api_request[4]
 
 
 @pytest.mark.parametrize('api_request', ['api/users'])
@@ -35,13 +35,54 @@ def test_api_post(api_client_post_value, api_request, json_request):
     assert api_response["job"] == json_request["job"]
 
 
+@pytest.mark.parametrize('api_request', ['api/users'])
+@pytest.mark.parametrize('json_request', [{"name": "morpheus","job": "zion resident"}])
+def test_api_put(api_client_put, api_request, json_request):
+    api_response = api_client_put
+    assert api_response["name"] == json_request["name"]
+    assert api_response["job"] == json_request["job"]
 
+# patch
+@pytest.mark.parametrize('api_request', ['api/users'])
+@pytest.mark.parametrize('json_request', [{"name": "morpheus","job": "zion resident"}])
+def test_api_patch(api_client_patch, api_request, json_request):
+    api_response = api_client_patch
+    assert api_response["name"] == json_request["name"]
+    assert api_response["job"] == json_request["job"]
 
+# Delete
+@pytest.mark.parametrize('api_request', ['api/users'])
+def test_api_patch(api_client_patch, api_request):
+    api_response = api_client_patch
+    assert api_response == 204
 
+# REGISTER - SUCCESSFUL
+@pytest.mark.parametrize('api_request', ['api/register'])
+@pytest.mark.parametrize('json_request', [{"email": "eve.holt@reqres.in","password": "pistol"}])
+def test_api_register_successful(api_client_register, api_request, json_request):
+    api_response = api_client_register
+    assert api_response["token"] == 'QpwL5tke4Pnpja7X4'
 
+# REGISTER - UNSUCCESSFUL
+@pytest.mark.parametrize('api_request', ['api/register'])
+@pytest.mark.parametrize('json_request', [{"email": "sydney@fife"}])
+def test_api_unsuccessful_register(api_client_unsuccessful_register, api_request, json_request):
+    api_response = api_client_unsuccessful_register
+    assert api_response["error"] == 'Missing password'
 
+# LOGIN - SUCCESSFUL
+@pytest.mark.parametrize('api_request', ['api/login'])
+@pytest.mark.parametrize('json_request', [{"email": "eve.holt@reqres.in","password": "cityslicka"}])
+def test_api_login_successful(api_client_login, api_request, json_request):
+    api_response = api_client_login
+    assert api_response["token"] == 'QpwL5tke4Pnpja7X4'
 
-
+# LOGIN - UNSUCCESSFUL
+@pytest.mark.parametrize('api_request', ['api/login'])
+@pytest.mark.parametrize('json_request', [{"email": "peter@klaven"}])
+def test_api_login_successful(api_client_login_fail, api_request, json_request):
+    api_response = api_client_login_fail
+    assert api_response["error"] == 'Missing password'
 
 
 # response = requests.get('https://reqres.in/api/users?page=2')
--- 8_API/4_reqres/conftest.py
+++ 8_API/4_reqres/conftest.py
@@ -2,30 +2,72 @@ import pytest
 import requests
 import json
 
-# @pytest.fixture()
-# def api_client_get_status(api_request):
-#     r = requests.get("https://reqres.in/"+str(api_request[0])+"")
-#     # this make dict
-#     # r = r.json()
-#     r = r.status_code
-#     return r == api_request[1]
+@pytest.fixture()
+def api_client_get_status(api_request):
+    r = requests.get("https://reqres.in/"+str(api_request[0])+"")
+    # this make dict
+    # r = r.json()
+    r = r.status_code
+    return r == api_request[1]
 
 
-# @pytest.fixture()
-# def api_client_get_value(api_request):
-#     r = requests.get("https://reqres.in/"+str(api_request[0])+"")
-#     response_body = r.json()
-#     return response_body
+@pytest.fixture()
+def api_client_get_value(api_request):
+    r = requests.get("https://reqres.in/"+str(api_request[0])+"")
+    response_body = r.json()
+    return response_body
 
 @pytest.fixture()
 def api_client_post_value(api_request, json_request):
     r = requests.post("https://reqres.in/"+str(api_request)+"", json_request)
     response_body = r.json()
     return response_body
+#
+#
+@pytest.fixture()
+def api_client_put(api_request, json_request):
+    r = requests.put("https://reqres.in/"+str(api_request)+"", json_request)
+    response_body = r.json()
+    print(response_body)
+    return response_body
+#
+# @pytest.fixture()
+def api_client_patch(api_request, json_request):
+    r = requests.patch("https://reqres.in/"+str(api_request)+"", json_request)
+    response_body = r.json()
+    print(response_body)
+    return response_body
+#
+# Delete
+@pytest.fixture()
+def api_client_patch(api_request):
+    r = requests.delete("https://reqres.in/"+str(api_request)+"")
+    return r.status_code
+#
+# REGISTER - SUCCESSFUL
+@pytest.fixture()
+def api_client_register(api_request, json_request):
+    r = requests.post("https://reqres.in/"+str(api_request)+"", json_request)
+    response_body = r.json()
+    return response_body
+#
+# REGISTER - UNSUCCESSFUL
+@pytest.fixture()
+def api_client_unsuccessful_register(api_request, json_request):
+    r = requests.post("https://reqres.in/"+str(api_request)+"", json_request)
+    response_body = r.json()
+    return response_body
+#
+# LOGIN - SUCCESSFUL
+@pytest.fixture()
+def api_client_login(api_request, json_request):
+    r = requests.post("https://reqres.in/"+str(api_request)+"", json_request)
+    response_body = r.json()
+    return response_body
 
-
-
-# print('response.headers == ' + str(response.headers["Content-Type"]))  # application/json; charset=utf-8
-
-# def api_client(api_request, json_request):
-# r = requests.post("https://reqres.in/"+str(api_request)+"", json={json_request})
+#
+@pytest.fixture()
+def api_client_login_fail(api_request, json_request):
+    r = requests.post("https://reqres.in/"+str(api_request)+"", json_request)
+    response_body = r.json()
+    return response_body
