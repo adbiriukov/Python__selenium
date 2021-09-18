8055938af8227011c664bdb3ee915c15ab62aefb
adbiriukov <adbiriukov@gmail.com>
2021-09-18 22:52:38 +0500

    added some tests

--- 8_API/7_herokuapp/conftest.py
+++ 8_API/7_herokuapp/conftest.py
@@ -10,23 +10,16 @@ def api_auth_post(api_request, json_request, headers):
     r = r.status_code
     return r
 
-
-
-
-
-
-
-
 # # # To get value
-# @pytest.fixture()
-# def api_client_get_value(api_request):
-#     r = requests.get("https://jsonplaceholder.typicode.com/"+str(api_request[0])+"")
-#     r = r.json()
-#     return r
-#
-#
+@pytest.fixture()
+def api_client_get_value(api_request, json_request):
+    r = requests.get("https://restful-booker.herokuapp.com/"+str(api_request[0])+"", json_request)
+    r = r.json()
+    return r
+
 # # To post
-# @pytest.fixture()
-# def api_client_post(api_request, json_request, headers):
-#     r = requests.post("https://gorest.co.in/"+str(api_request)+"", json_request, headers)
-#     return r.json()
+@pytest.fixture()
+def api_client_post(api_request, json_request):
+    r = requests.post("https://restful-booker.herokuapp.com/"+str(api_request)+"", data=json_request)
+    r = r.status_code
+    return r
--- 8_API/7_herokuapp/herokuapp_api_tests.py
+++ 8_API/7_herokuapp/herokuapp_api_tests.py
@@ -11,13 +11,44 @@ import pytest
 #     print(r)
 #     assert r == int(api_request[1])
 
-# auth by token
-@pytest.mark.parametrize('api_request', [['auth', '200']])
-@pytest.mark.parametrize('json_request', [{'method': 'POST', 'token': 'abc123'}])
-@pytest.mark.parametrize('headers', [{'Content-type': 'application/json'}])
-def test_api_auth_post(api_auth_post, api_request, json_request, headers):
-    r = api_auth_post
-    print('==========')
-    print(r)
-    assert r == int(api_request[1])
+# # auth by token
+# @pytest.mark.parametrize('api_request', [['auth', '200']])
+# @pytest.mark.parametrize('json_request', [{'method': 'POST', 'token': 'abc123'}])
+# @pytest.mark.parametrize('headers', [{'Content-type': 'application/json'}])
+# def test_api_auth_post(api_auth_post, api_request, json_request, headers):
+#     r = api_auth_post
+#     print('==========')
+#     print(r)
+#     assert r == int(api_request[1])
 
+
+# @pytest.mark.parametrize('api_request', [['booking']])
+# @pytest.mark.parametrize('json_request', [{'method': 'GET', 'firstname': 'Sally'}])
+# def test_api_get_value(api_client_get_value, api_request, json_request):
+#     r = api_client_get_value
+#     print(r)
+
+# to get specific booking
+# @pytest.mark.parametrize('api_request', [['booking/1']])
+# @pytest.mark.parametrize('json_request', [{'method': 'GET', 'lastname': 'Brown'}])
+# def test_api_get_value(api_client_get_value, api_request, json_request):
+#     r = api_client_get_value
+#     print(r)
+
+# Create booking
+@pytest.mark.parametrize('api_request', [['booking/1']])
+@pytest.mark.parametrize('json_request', [{
+    "firstname" : "Jim",
+    "lastname" : "Brown",
+    "totalprice" : 111,
+    "depositpaid" : True,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}])
+@pytest.mark.parametrize('headers', [{'Content-type': 'application/json'}])
+def test_api_get_value(api_client_post, api_request, json_request, headers):
+    r = api_client_post
+    print(r)
