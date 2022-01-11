d4c8ab1ed7150aa650f8e21c6df453f53cfe5cbf
adbiriukov <adbiriukov@gmail.com>
2022-01-11 23:33:57 +0500

    finished petstore tests

--- 8_API/8_petstore/conftest.py
+++ 8_API/8_petstore/conftest.py
@@ -4,93 +4,23 @@ from random import choice
 
 
 @pytest.fixture()
-def api_get_pet(api_request, headers):
-    # r = requests.get("https://"+str(login_pass)+"@restful-booker.herokuapp.com/"+str(api_request[0])+"")
+def api_get(api_request, headers):
     r = requests.get("https://petstore.swagger.io/"+str(api_request[0])+"", headers)
-    r = r.status_code
-    return r
-
-
+    return r.status_code
 
 @pytest.fixture()
-def api_post_order(api_request, data, headers):
+def api_post(api_request, data, headers):
     r = requests.post("https://petstore.swagger.io/"+str(api_request[0])+"", json=data, headers=headers)
-    r = r.json()
-    return r
-#
-#
-# # # To post
-# @pytest.fixture()
-# def api_client_post(generate_booking):
-#     r = requests.post("https://restful-booker.herokuapp.com/booking/", json=generate_booking)
-#     print(generate_booking)
-#     r = r.status_code
-#     return r
-#
-#
-# # Update booking
-# @pytest.fixture()
-# def api_client_put(get_authtoken, generate_booking, firstname='Jim', lastname='Brown', totalprice=111, depositpaid=True,
-#                    checkin='2018-01-01', additionalneeds='Breakfast'):
-#     r = requests.post("https://restful-booker.herokuapp.com/booking/", json=generate_booking)
-#     booking_id = r.json()['bookingid']
-#     r = requests.put('https://restful-booker.herokuapp.com/booking/{:d}/'.format(booking_id), json={
-#         "firstname": firstname,
-#         "lastname": lastname,
-#         "totalprice": totalprice,
-#         "depositpaid": depositpaid,
-#         "bookingdates": {
-#             "checkin": checkin,
-#             "checkout": checkin
-#         },
-#         "additionalneeds": additionalneeds
-#     }, cookies={
-#         "token": get_authtoken
-#     })
-#     return r.json()
-#
-#
-# @pytest.fixture()
-# def remove_booking(generate_booking, get_authtoken):
-#     # Generate new booking to get id
-#     r = requests.post("https://restful-booker.herokuapp.com/booking/", json=generate_booking)
-#     booking_id = r.json()['bookingid']
-#     # Remove booking
-#     return requests.delete('https://restful-booker.herokuapp.com/booking/{:d}/'.format(booking_id), cookies={
-#         "token": get_authtoken
-#     })
-#
-#
-# @pytest.fixture()
-# def get_authtoken(username='admin', password='password123'):
-#     url = "https://restful-booker.herokuapp.com/auth"
-#     return requests.post(url, json={
-#         "username": username,
-#         "password": password
-#     }).json()['token']
-#
-#
-# # Generate new booking
-# @pytest.fixture()
-# def generate_booking():
-#     firstname = choice(['Bart', 'Maggie', 'Lisa', 'Marge', 'Homer'])
-#     lastname = choice(['Simpson', 'Smithers', 'Burns', 'Krustofsky'])
-#     totalprice = choice([112, 113, 114, 115, 116, 117, 300, 999])
-#     depositpaid = choice([True, False])
-#     checkin = choice(['2018-02-02', '2018-03-03', '2018-04-04', '2018-05-05', '2018-06-11'])
-#     checkout = choice(['2018-02-7', '2018-03-08', '2018-04-09', '2018-05-10', '2018-06-21'])
-#     additionalneeds = choice(['Breakfast', 'Mini Fridge', 'Extra towel', 'Dinner'])
-#
-#     return (
-#         {
-#             "firstname": firstname,
-#             "lastname": lastname,
-#             "totalprice": totalprice,
-#             "depositpaid": depositpaid,
-#             "bookingdates": {
-#                 "checkin": checkin,
-#                 "checkout": checkout
-#             },
-#             "additionalneeds": additionalneeds
-#         }
-#     )
+    return r.json()
+
+@pytest.fixture()
+def api_delete(api_request, headers):
+    r = requests.delete("https://petstore.swagger.io/"+str(api_request[0])+"", headers)
+    return r.status_code
+
+@pytest.fixture()
+def api_put(api_request, data, headers):
+    r = requests.put("https://petstore.swagger.io/"+str(api_request[0])+"", json=data, headers=headers)
+    return r.json()
+
+
--- 8_API/8_petstore/petstore_api_tests.py
+++ 8_API/8_petstore/petstore_api_tests.py
@@ -1,70 +1,137 @@
 import pytest
 
-
-@pytest.mark.parametrize('api_request', [['v2/store/inventory', '200']])
-@pytest.mark.parametrize('headers', [{'accept': 'application/json'}])
-def test_get_pet(api_get_pet, api_request, headers):
-    r = api_get_pet
-    print()
-    print(r)
-    assert r == int(api_request[1])
-
-
-@pytest.mark.parametrize('api_request', [['v2/store/order']])
-@pytest.mark.parametrize('headers', [{'Content-Type': 'application/json', 'accept': 'application/json'}])
-@pytest.mark.parametrize('data', [{
-  "id": 0,
-  "petId": 0,
-  "quantity": 0,
-  "shipDate": "2021-11-25T14:54:06.445Z",
-  "status": "placed",
-  "complete": True
-}])
-def test_api_auth_post(api_post_order, api_request, data, headers):
-    r = api_post_order
-    print()
-    print(r['complete'])
-    assert r['complete'] is True
-
-# # auth by token
-# @pytest.mark.parametrize('api_request', [['auth', '200']])
-# @pytest.mark.parametrize('json_request', [{'method': 'POST', 'token': 'abc123'}])
-# @pytest.mark.parametrize('headers', [{'Content-type': 'application/json'}])
-# def test_api_auth_post(api_auth_post, api_request, json_request, headers):
-#     r = api_auth_post
-#     print('==========')
-#     print(r)
+# # create new user
+# @pytest.mark.parametrize('api_request', [['v2/user', '200']])
+# @pytest.mark.parametrize('headers', [{'Content-Type': 'application/json', 'accept': 'application/json'}])
+# @pytest.mark.parametrize('data', [{
+#     "id": 0,
+#     "username": "User_name",
+#     "firstName": "f_User_name",
+#     "lastName": "l_User_name",
+#     "email": "User_name@gmail.com",
+#     "password": "qwerty",
+#     "phone": "1234567890",
+#     "userStatus": 0
+# }])
+# def test_create_new_user(api_post, api_request, data, headers):
+#     r = api_post
+#     print()
+#     # assert that http response == 200
+#     assert r['code'] == int(api_request[1])
+#
+# # User login
+# @pytest.mark.parametrize('api_request', [['v2/user/login?username=User_name&password=qwerty', '200']])
+# @pytest.mark.parametrize('headers', [{'accept': 'application/json'}])
+# def test_user_login(api_get, api_request, headers):
+#     r = api_get
+#     # assert that http response == 200
 #     assert r == int(api_request[1])
 #
+# # User logout
+# @pytest.mark.parametrize('api_request', [['v2/user/logout', '200']])
+# @pytest.mark.parametrize('headers', [{'accept': 'application/json'}])
+# def test_user_logout(api_get, api_request, headers):
+#     r = api_get
+#     # assert that http response == 200
+#     assert r == int(api_request[1])
 #
-# @pytest.mark.parametrize('api_request', [['booking']])
-# @pytest.mark.parametrize('json_request', [{'method': 'GET', 'firstname': 'Sally'}])
-# def test_api_get_value(api_client_get_value, api_request, json_request):
-#     r = api_client_get_value
-#     print(r)
-#
-#
-# # to get specific booking
-# @pytest.mark.parametrize('api_request', [['booking/1']])
-# @pytest.mark.parametrize('json_request', [{'method': 'GET', 'lastname': 'Brown'}])
-# def test_api_get_value(api_client_get_value, api_request, json_request):
-#     r = api_client_get_value
-#     print(r)
-#
-#
-# # Create booking
-# def test_api_greate_booking(api_client_post):
-#     r = api_client_post
-#     print(r)
-#
-#
-# # Update booking
-# def test_api_put(api_client_put):
-#     r = api_client_put
-#     print(r)
-#
-#
-# # Remove booking
-# def test_remove_booking(remove_booking):
-#     r = remove_booking
-#     print(r)
+# # Update user
+# @pytest.mark.parametrize('api_request', [['v2/user/User_name', '200']])
+# @pytest.mark.parametrize('headers', [{'Content-Type': 'application/json', 'accept': 'application/json'}])
+# @pytest.mark.parametrize('data', [{
+#     "id": 0,
+#     "username": "User_name",
+#     "firstName": "string",
+#     "lastName": "string",
+#     "email": "string",
+#     "password": "string",
+#     "phone": "string",
+#     "userStatus": 0
+# }])
+# def test_update_user(api_put, api_request, data, headers):
+#     r = api_put
+#     print()
+#     # assert that http response == 200
+#     assert r['code'] == int(api_request[1])
+
+# # Get info about user
+# @pytest.mark.parametrize('api_request', [['v2/user/User_name', '200']])
+# @pytest.mark.parametrize('headers', [{'accept': 'application/json'}])
+# def test_user_logout(api_get, api_request, headers):
+#     r = api_get
+#     # assert that http response == 200
+#     assert r == int(api_request[1])
+
+
+# # User delete
+# @pytest.mark.trylast
+# @pytest.mark.parametrize('api_request', [['v2/user/User_name', '200']])
+# @pytest.mark.parametrize('headers', [{'accept': 'application/json'}])
+# def test_user_delete(api_get, api_request, headers):
+#     r = api_get
+#     # assert that http response == 200
+#     assert r == int(api_request[1])
+
+
+# # Add a new pet to the store
+# @pytest.mark.parametrize('api_request', [['v2/pet', 'doggie']])
+# @pytest.mark.parametrize('headers', [{'Content-Type': 'application/json', 'accept': 'application/json'}])
+# @pytest.mark.parametrize('data', [{
+# "id": 0,
+#   "category": {
+#     "id": 0,
+#     "name": "string"
+#   },
+#   "name": "Doggie",
+#   "photoUrls": [
+#     "string"
+#   ],
+#   "tags": [
+#     {
+#       "id": 0,
+#       "name": "string"
+#     }
+#   ],
+#   "status": "available"
+# }])
+# def test_add_new_pet(api_put, api_request, data, headers):
+#     r = api_put
+#     # assert that name is 'Doggie'
+#     assert r['name'] == api_request[1]
+
+# # Update existing pet
+# @pytest.mark.parametrize('api_request', [['v2/pet', 'Doggie_2']])
+# @pytest.mark.parametrize('headers', [{'Content-Type': 'application/json', 'accept': 'application/json'}])
+# @pytest.mark.parametrize('data', [{
+# "id": 0,
+#   "category": {
+#     "id": 0,
+#     "name": "string"
+#   },
+#   "name": "Doggie_2",
+#   "photoUrls": [
+#     "string"
+#   ],
+#   "tags": [
+#     {
+#       "id": 0,
+#       "name": "string"
+#     }
+#   ],
+#   "status": "available"
+# }])
+# def test_add_new_pet(api_put, api_request, data, headers):
+#     r = api_put
+#     # assert that name is 'Doggie_2'
+#     assert r['name'] == api_request[1]
+
+# # 1) get list of pets with status 'pending'
+# # 2) find pet with id 1
+# @pytest.mark.parametrize('api_request', [['v2/pet/findByStatus?status=pending', '200'], ['v2/pet/2', '200']])
+# @pytest.mark.parametrize('headers', [{'accept': 'application/json'}])
+# def test_user_logout(api_get, api_request, headers):
+#     r = api_get
+#     # assert that http response == 200
+#     assert r == int(api_request[1])
+
+
