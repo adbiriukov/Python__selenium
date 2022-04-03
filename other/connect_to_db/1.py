9c5ec41f39de3734d6acc0d5805fdeccec4aad6c
adbiriukov <adbiriukov@gmail.com>
2022-04-03 23:28:45 +0500

    Update 1.py

--- other/connect_to_db/1.py
+++ other/connect_to_db/1.py
@@ -4,31 +4,31 @@ import pymysql
 conn = pymysql.connect(host='127.0.0.1', port=3306, user='bn_opencart', password='', db='bitnami_opencart')
 cursor = conn.cursor()
 
-# # Print all tables
-# cursor.execute('SHOW TABLES')
-# myresult = cursor.fetchall()
-# print(myresult)
-#
-# # show all in table oc_currency
-# cursor.execute('select * from oc_currency')
-# table = cursor.fetchall()
-# print(table)
-#
-# # show all in table oc_product
-# cursor.execute('select * from oc_product')
-# table = cursor.fetchall()
-# print(table)
-# print('============')
-#
-# # show first row of table oc_product
-# cursor.execute('select * from oc_product')
-# fetch_one = cursor.fetchone()
-# print(fetch_one)
+# Print all tables
+cursor.execute('SHOW TABLES')
+myresult = cursor.fetchall()
+print(myresult)
 
-# # Describe table oc_product
-# cursor.execute('describe oc_product')
-# fetch_one = cursor.fetchall()
-# print(fetch_one)
+# show all in table oc_currency
+cursor.execute('select * from oc_currency')
+table = cursor.fetchall()
+print(table)
+
+# show all in table oc_product
+cursor.execute('select * from oc_product')
+table = cursor.fetchall()
+print(table)
+print('============')
+
+# show first row of table oc_product
+cursor.execute('select * from oc_product')
+fetch_one = cursor.fetchone()
+print(fetch_one)
+
+# Describe table oc_product
+cursor.execute('describe oc_product')
+fetch_one = cursor.fetchall()
+print(fetch_one)
 
 # Show 2 most expensive models from oc_product table
 cursor.execute(' select model, price from oc_product ORDER BY Price DESC')
